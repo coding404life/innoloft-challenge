386c62e02c1e1ad85ac32669ef041454fb59fb64
coding404life <coding404life@gmail.com>
2023-09-04 15:53:53 +0300

    init innoloft-challenge project

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { store } from './app/store';
+import { Provider } from 'react-redux';
+import './styles/index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
